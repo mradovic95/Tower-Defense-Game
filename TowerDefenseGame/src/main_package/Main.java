12bd5f16503b209e1277917821c894384ae726c8
jovan <jovan@jovan-PC>
2016-10-30 21:29:27 +0100

    asd

--- TowerDefenseGame/src/main_package/Main.java
+++ TowerDefenseGame/src/main_package/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//"da li vidis ovo"
+		//"da li vidis ovo"
+		//"da li vidis ovo"
+		//"da li vidis ovo"
+		//"da li vidis ovo"
 	}
 
 }
